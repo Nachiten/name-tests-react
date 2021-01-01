62d2c708a76488df760b2f1f22a763e7f26b6848
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2021-01-01 20:11:16 -0300

    Terminado Capitulo 86, split app into components

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+
+    let clasesAsignadas = [];
+    const cantidadPersonas = props.persons.length;
+    let buttonClass = [classes.Button];
+
+    if (props.mostrarPersonas){
+        buttonClass.push(classes.Red);
+    }
+
+    if (cantidadPersonas <= 2) {
+       clasesAsignadas.push(classes.red);
+    }
+
+    if (cantidadPersonas <= 1) {
+       clasesAsignadas.push(classes.bold);
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1 onCopy={this.seCopiaronDatosHandler}>Titulo de la pagina</h1>
+            <p className={clasesAsignadas.join(' ')}>Esto funciona!!!</p>
+            <button className={buttonClass.join(' ')} onClick={props.clicked}>
+                {
+                    props.mostrarPersonas ?
+                        <p>Ocultar nombres</p> : <p>Mostrar Nombres</p>
+                }
+            </button>
+        </div>
+    );
+};
+
+export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
@@ -16,47 +17,25 @@ class App extends Component {
    render() {
 
       let persons = null;
-      let buttonClass = [classes.Button];
-
+      
       if (this.state.mostrarPersonas) {
          persons = (
             <div>
-               {this.state.persons.map((person, index) => {
-                  return <Person
-                     click={() => this.eliminarPersonaHandler(index)}
-                     name={person.name}
-                     age={person.age}
-                     key={person.id}
-                     changed={(event) => this.cambiarNombreHandler(event, person.id)}
-                  />
-               })}
+               <Persons 
+               persons={this.state.persons} 
+               clicked={this.eliminarPersonaHandler} 
+               changed={this.cambiarNombreHandler}/>
             </div>
          );
-
-         buttonClass.push(classes.Red);
-      }
-
-      let clasesAsignadas = [];
-      const cantidadPersonas = this.state.persons.length;
-
-      if (cantidadPersonas <= 2) {
-         clasesAsignadas.push(classes.red);
-      }
-
-      if (cantidadPersonas <= 1) {
-         clasesAsignadas.push(classes.bold);
       }
 
       return (
          <div className={classes.App}>
-            <h1 onCopy={this.seCopiaronDatosHandler}>Titulo de la pagina</h1>
-            <p className={clasesAsignadas.join(' ')}>Esto funciona!!!</p>
-            <button className={buttonClass.join(' ')} onClick={this.esconderPersonaHandler}>
-               {
-                  this.state.mostrarPersonas ?
-                     <p>Ocultar nombres</p> : <p>Mostrar Nombres</p>
-               }
-            </button>
+            <Cockpit
+               mostrarPersonas={this.state.mostrarPersonas}
+               persons={this.state.persons}
+               clicked={this.esconderPersonaHandler}
+            />
             {persons}
          </div>
       );
